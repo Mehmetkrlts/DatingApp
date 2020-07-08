62e3a09c15ff1572fa29b8044b33941638a84ff0
Mehmetkrlts <63557811+Mehmetkrlts@users.noreply.github.com>
2020-07-08 15:11:13 +0300

    Security[Module 3]

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,3 +1,4 @@
+using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 using Models;
 
@@ -10,5 +11,7 @@ namespace Data
         
 
         public DbSet<Value> Values { get; set; }
+
+        public DbSet<User> Users {get ; set ; }
     }
 }
