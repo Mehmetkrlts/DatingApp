a4c9815b3c5131e5fd371044bd4f4ec61992b3ae
Mehmetkrlts <63557811+Mehmetkrlts@users.noreply.github.com>
2020-07-22 00:40:10 +0300

    Paging and User Sorting [Module 14]

--- DatingApp-SPA/src/app/_resolver/member-list.resolver.ts
+++ DatingApp-SPA/src/app/_resolver/member-list.resolver.ts
@@ -1,25 +1,28 @@
-import { Injectable } from "@angular/core";
-import { User } from "../_models/user";
-import { Resolve, Router, ActivatedRouteSnapshot } from "@angular/router";
-import { UserService } from "../_Services/user.service";
-import { AlertifyService } from "../_Services/alertify.service";
-import { Observable, of } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { User } from '../_models/user';
+import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { UserService } from '../_Services/user.service';
+import { AlertifyService } from '../_Services/alertify.service';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 
 export class MemberListResolver implements Resolve<User[]> {
-    constructor(private userService: UserService,private router: Router,
+    pageNumber = 1 ;
+    pageSize = 5 ;
+
+    constructor(private userService: UserService, private router: Router,
         private alertify: AlertifyService) {}
 
         resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-            return this.userService.getUsers().pipe(
+            return this.userService.getUsers(this.pageNumber , this.pageSize).pipe(
                 catchError(error => {
                     this.alertify.error('Problem retrieving data') ;
-                    this.router.navigate(['/home'])
+                    this.router.navigate(['/home']);
                     return of(null) ;
                 })
             );
         }
-    
+
 }
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule } from '@angular/common/http' ;
 import {FormsModule, ReactiveFormsModule} from '@angular/forms' ;
-import {BsDropdownModule, TabsModule, BsDatepickerModule} from 'ngx-bootstrap' ;
+import {BsDropdownModule, TabsModule, BsDatepickerModule, PaginationModule, ButtonsModule} from 'ngx-bootstrap' ;
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import {NgxGalleryModule} from 'ngx-gallery' ;
@@ -58,11 +58,13 @@ export function tokenGetter () {
       BrowserModule,
       HttpClientModule,
       FormsModule,
+      ButtonsModule.forRoot(),
       FileUploadModule,
       BsDropdownModule.forRoot(),
       BsDatepickerModule.forRoot(),
       ReactiveFormsModule,
       NgxGalleryModule,
+      PaginationModule.forRoot(),
       TabsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
       JwtModule.forRoot({
